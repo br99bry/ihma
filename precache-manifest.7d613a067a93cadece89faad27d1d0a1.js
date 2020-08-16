5523ff41669e9e077b4f16d8cbfaaaefb31c60c6
Bryan <bryanblack1999@gmail.com>
2020-08-16 16:23:07 -0500

    Updates

---
+++ precache-manifest.7d613a067a93cadece89faad27d1d0a1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c36bab165f37352486a926da70a9b6",
+    "url": "/ihma/index.html"
+  },
+  {
+    "revision": "4cc4eba5f996c6269f5d",
+    "url": "/ihma/static/css/main.cb34c7df.chunk.css"
+  },
+  {
+    "revision": "92101355277014653f33",
+    "url": "/ihma/static/js/2.09477f13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ihma/static/js/2.09477f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc4eba5f996c6269f5d",
+    "url": "/ihma/static/js/main.22c6c72f.chunk.js"
+  },
+  {
+    "revision": "575b84491f6bc552df76",
+    "url": "/ihma/static/js/runtime-main.68f2be7b.js"
+  },
+  {
+    "revision": "aeea265d924e67a21f3fc272001cc1e1",
+    "url": "/ihma/static/media/DiegoMax.aeea265d.png"
+  },
+  {
+    "revision": "44000f82584522a1d933234a92dd3942",
+    "url": "/ihma/static/media/JesusMendoza.44000f82.png"
+  },
+  {
+    "revision": "c6efab94b0d3bd82dd08c55f33ae2608",
+    "url": "/ihma/static/media/JorgeArias.c6efab94.png"
+  },
+  {
+    "revision": "07efbe65344ec9adb3b901dad7db4475",
+    "url": "/ihma/static/media/JorgeAriasRedondo.07efbe65.png"
+  },
+  {
+    "revision": "56e7afa7c3477f54aee9d920e69902ec",
+    "url": "/ihma/static/media/Logo.56e7afa7.svg"
+  },
+  {
+    "revision": "11ccc365736fdaa3ae9a07010996d6bb",
+    "url": "/ihma/static/media/MauricioCampos.11ccc365.png"
+  },
+  {
+    "revision": "293642f0c5028fba20db456e28ae9d5a",
+    "url": "/ihma/static/media/MauricioCamposRedondo.293642f0.png"
+  },
+  {
+    "revision": "76230f72b5fdca7f1200f4a943575347",
+    "url": "/ihma/static/media/angelMexico.76230f72.png"
+  },
+  {
+    "revision": "8f2a7330771c337ec6f0b868b77dbd7c",
+    "url": "/ihma/static/media/areasDecorateLittle.8f2a7330.png"
+  },
+  {
+    "revision": "ac046721c65815208d96d39a69478064",
+    "url": "/ihma/static/media/clientesDecorate.ac046721.png"
+  },
+  {
+    "revision": "27b3aeeba7f8a4d2bcd2fd39662580e4",
+    "url": "/ihma/static/media/clientesDecorateLittle.27b3aeeb.png"
+  },
+  {
+    "revision": "2d4ce33b5633a85ed9fa63bf365e6f0a",
+    "url": "/ihma/static/media/grisTextura.2d4ce33b.png"
+  },
+  {
+    "revision": "a3d3350df2bfcd567b75695f060166c4",
+    "url": "/ihma/static/media/leftEquipoLegal.a3d3350d.png"
+  },
+  {
+    "revision": "5ee1373c1bdffd3776bade8f137be6d7",
+    "url": "/ihma/static/media/logoHero.5ee1373c.png"
+  },
+  {
+    "revision": "aea193fc4c159b50a274dba7aefea50d",
+    "url": "/ihma/static/media/mainBottom.aea193fc.png"
+  },
+  {
+    "revision": "a6c07d37ab25aacddfeaea07e0e5ced3",
+    "url": "/ihma/static/media/mapa.a6c07d37.png"
+  },
+  {
+    "revision": "67e420047e5e8e978cd1167130e5ab4f",
+    "url": "/ihma/static/media/socioDecoration.67e42004.png"
+  },
+  {
+    "revision": "26134d560adac725901ed5a4330112ad",
+    "url": "/ihma/static/media/socioDecorationLittle.26134d56.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f25bb163d1d5665f6ab0ee95aadb7bb8.js"
+  "/ihma/precache-manifest.7d613a067a93cadece89faad27d1d0a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ihma/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
